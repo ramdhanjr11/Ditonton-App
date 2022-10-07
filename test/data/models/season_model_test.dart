8592eb2a72d94ba5a0f3f6dc72d8e853db7fc266
M Ramdhan Syahputra <ramdhanjr690@gmail.com>
2022-10-07 17:09:56 +0700

    add season model testing

--- test/data/models/season_model_test.dart
+++ test/data/models/season_model_test.dart
@@ -1,1 +1,30 @@
-void main() {}
+import 'package:ditonton/data/models/season_model.dart';
+import 'package:ditonton/domain/entities/season.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  final tSeasonModel = SeasonModel(
+    airDate: "airdate",
+    episodeCount: 3,
+    id: 1,
+    name: "season model",
+    overview: "overview",
+    posterPath: "poster path",
+    seasonNumber: 8,
+  );
+
+  final tSeason = Season(
+    airDate: "airdate",
+    episodeCount: 3,
+    id: 1,
+    name: "season model",
+    overview: "overview",
+    posterPath: "poster path",
+    seasonNumber: 8,
+  );
+
+  test("should be a sub of season entity", () {
+    final result = tSeasonModel.toEntity();
+    expect(result, tSeason);
+  });
+}
